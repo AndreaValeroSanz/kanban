4eabc47c6977f44c409b381279fffce7b0b1d531
AlbertoCamachoSampedro <albertocamachosampedro@gmail.com>
2024-10-09 11:52:43 +0200

    Added comentaries for better understanding

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -5,25 +5,35 @@ import GroupName from "./MenuLateral/GroupName.jsx";
 import MyProjects from "./MenuLateral/MyProjects/MyProjects.jsx";
 function Home() {
   return (
+    /* Contenedor Principal  */
     <div className="px-5 ">
+      
+      {/* Contenedor superior */}
       <div className="row ">
-        {/* Menu lateral */}
+
+        {/* Contenedor Logo */}
         <div className="menu col-lg-2 topbar BorderRight">
           <GroupName />
         </div>
 
-        {/* Barra de navegación */}
+        {/* Barra de navegación (Búsqueda + Usuario)*/}
         <div className="col-lg-10 topbar">
           <Navbar />
         </div>
-      </div>
 
+      </div>
+      
+      {/* Contenedor inferior */}
       <div className="row">
+
+        {/* Menú lateral (Navegación + Lista_Proyectos)*/}
         <div className="py-3 col-lg-2 BorderRight">
           <Menu />
           <MyProjects />
         </div>
+
       </div>
+
     </div>
   );
 }
